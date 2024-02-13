7572cf6d34554c2cc3f323cda3987a2e5db37733
Lawrence Kern <law@lawkern.com>
2024-02-12 23:57:02 -0500

    Implement prototype window movement and resizing with SDL

---
+++ code/graphics/build.bat
@@ -0,0 +1,27 @@
+@ECHO off
+
+SET DEVELOPMENT_BUILD=1
+
+SET WARNING_FLAGS=-WX -W4 -wd4201 -wd4204
+IF %DEVELOPMENT_BUILD%==1 (
+   SET WARNING_FLAGS=%WARNING_FLAGS% -wd4100 -wd4101 -wd4189
+)
+
+REM   NOTE(law): Specify the path to the SDL2 include and lib directories
+REM   below. Additionally, a copy of the file SDL2.dll must be present in the
+REM   build directory in order to run the compiled executable.
+
+SET INCLUDE=D:\SDL2\include;%INCLUDE%
+SET LIB=D:\SDL2\lib\x64;%LIB%
+
+SET COMPILER_FLAGS=-nologo -Z7 -Oi -Od -FC -MT -diagnostics:column %WARNING_FLAGS%
+SET COMPILER_FLAGS=%COMPILER_FLAGS% -DDEVELOPMENT_BUILD=%DEVELOPMENT_BUILD% -DSDL_MAIN_HANDLED=1 -D_CRT_SECURE_NO_WARNINGS
+
+SET LINKER_FLAGS=-opt:ref -incremental:no sdl2.lib sdl2main.lib
+
+IF NOT EXIST ..\..\build mkdir ..\..\build
+PUSHD ..\..\build
+
+cl ..\code\graphics\platform_sdl.c %COMPILER_FLAGS% -Fegfx /link %LINKER_FLAGS%
+
+POPD
---
+++ code/graphics/platform_sdl.c
@@ -0,0 +1,160 @@
+/* /////////////////////////////////////////////////////////////////////////// */
+/* (c) copyright 2024 Lawrence D. Kern /////////////////////////////////////// */
+/* /////////////////////////////////////////////////////////////////////////// */
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <SDL.h>
+
+#include "graphics.c"
+
+#define SDL_GET_SECONDS_ELAPSED(start, end) ((float)((end) - (start)) / (float)(frame_counter_frequency))
+
+int main(int argument_count, char **arguments)
+{
+   if(SDL_Init(SDL_INIT_VIDEO))
+   {
+      exit(1);
+   }
+
+   SDL_ShowCursor(SDL_DISABLE);
+
+   struct texture backbuffer = {960, 540};
+   // struct texture backbuffer = {1920, 1080};
+   // struct texture backbuffer = {2560, 1440};
+   backbuffer.memory = malloc(backbuffer.width * backbuffer.height * sizeof(u32));
+
+   SDL_Window *window = SDL_CreateWindow("EXO GFX", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, backbuffer.width, backbuffer.height, 0); // SDL_WINDOW_RESIZABLE
+   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+   SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, backbuffer.width, backbuffer.height);
+
+   printf("Resolution: %dx%d\n", backbuffer.width, backbuffer.height);
+
+   int target_refresh_rate = 60;
+   SDL_DisplayMode mode;
+   if(SDL_GetDesktopDisplayMode(SDL_GetWindowDisplayIndex(window), &mode) == 0 && mode.refresh_rate != 0)
+   {
+      target_refresh_rate = mode.refresh_rate;
+   }
+   printf("Target refresh rate: %d\n", target_refresh_rate);
+   float target_seconds_per_frame = 1.0f / target_refresh_rate;
+
+   struct input input = {0};
+
+   struct storage storage = {0};
+   storage.size = 512 * 1024;
+   storage.memory = malloc(storage.size);
+
+   u64 frame_count = 0;
+   u64 frame_counter_frequency = SDL_GetPerformanceFrequency();
+   u64 frame_start_counter = SDL_GetPerformanceCounter();
+
+   bool is_fullscreen = false;
+   bool is_running = true;
+   while(is_running)
+   {
+      for(u32 button_index = 0; button_index < MOUSE_BUTTON_COUNT; ++button_index)
+      {
+         input.mouse_buttons[button_index].changed_state = false;
+      }
+
+      SDL_Event event;
+      while(SDL_PollEvent(&event))
+      {
+         switch(event.type)
+         {
+            case SDL_QUIT:
+            {
+               is_running = false;
+            } break;
+
+            case SDL_MOUSEBUTTONDOWN:
+            case SDL_MOUSEBUTTONUP:
+            {
+               printf("Button %d at x:%d, y:%d\n", event.button.button, event.button.x, event.button.y);
+
+               struct input_state *mouse_button = 0;
+               switch(event.button.button)
+               {
+                  case SDL_BUTTON_LEFT:   {mouse_button = input.mouse_buttons + MOUSE_BUTTON_LEFT;} break;
+                  case SDL_BUTTON_MIDDLE: {mouse_button = input.mouse_buttons + MOUSE_BUTTON_MIDDLE;} break;
+                  case SDL_BUTTON_RIGHT:  {mouse_button = input.mouse_buttons + MOUSE_BUTTON_RIGHT;} break;
+                  case SDL_BUTTON_X1:     {mouse_button = input.mouse_buttons + MOUSE_BUTTON_X1;} break;
+                  case SDL_BUTTON_X2:     {mouse_button = input.mouse_buttons + MOUSE_BUTTON_X2;} break;
+               }
+
+               assert(mouse_button);
+               mouse_button->changed_state = true;
+               if(event.button.state == SDL_PRESSED)
+               {
+                  mouse_button->is_pressed = true;
+               }
+               else
+               {
+                  assert(event.button.state == SDL_RELEASED);
+                  mouse_button->is_pressed = false;
+               }
+            } break;
+
+            case SDL_KEYDOWN:
+            case SDL_KEYUP:
+            {
+               bool is_alt_pressed = (event.key.keysym.mod & KMOD_ALT);
+
+               SDL_Keycode code = event.key.keysym.sym;
+               if(event.key.state == SDL_PRESSED)
+               {
+                  if(code == SDLK_ESCAPE || (is_alt_pressed && code == SDLK_F4))
+                  {
+                     is_running = false;
+                  }
+                  else if(is_alt_pressed && code == SDLK_RETURN)
+                  {
+                     SDL_SetWindowFullscreen(window, (is_fullscreen) ? 0 : SDL_WINDOW_FULLSCREEN_DESKTOP);
+                     is_fullscreen = !is_fullscreen;
+                  }
+               }
+            } break;
+         }
+      }
+
+      int mousex, mousey, window_width, window_height;
+      SDL_GetMouseState(&mousex, &mousey);
+      SDL_GetWindowSize(window, &window_width, &window_height);
+      input.mouseu = (float)mousex / (float)window_width;
+      input.mousev = (float)mousey / (float)window_height;
+
+      update(&backbuffer, &input, &storage);
+
+      SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0xFF, 0xFF);
+      SDL_RenderClear(renderer);
+      SDL_UpdateTexture(texture, 0, backbuffer.memory, backbuffer.width * sizeof(u32));
+      SDL_RenderCopy(renderer, texture, 0, 0);
+      SDL_RenderPresent(renderer);
+
+      u32 sleep_ms = 0;
+      u64 frame_end_counter = SDL_GetPerformanceCounter();
+      float frame_seconds_elapsed = SDL_GET_SECONDS_ELAPSED(frame_start_counter, frame_end_counter);
+      if(frame_seconds_elapsed < target_seconds_per_frame)
+      {
+         sleep_ms = (u32)(((target_seconds_per_frame - frame_seconds_elapsed) * 1000.0f) - 1);
+         SDL_Delay(sleep_ms);
+      }
+      while(frame_seconds_elapsed < target_seconds_per_frame)
+      {
+         frame_end_counter = SDL_GetPerformanceCounter();
+         frame_seconds_elapsed = SDL_GET_SECONDS_ELAPSED(frame_start_counter, frame_end_counter);
+      }
+      if((frame_count++ % target_refresh_rate) == 0)
+      {
+         printf("%.04f ms\n", (1000.0f * frame_seconds_elapsed));
+      }
+      frame_start_counter = frame_end_counter;
+   }
+
+   return(0);
+}
