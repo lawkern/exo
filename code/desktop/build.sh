64873a08208ad9cfa960a1076d069ed297ce4db4
law <law@lawkern.com>
2024-08-13 22:56:32 -0500

    Update Linux build for new project layout

--- code/desktop/build.sh
+++ code/desktop/build.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-DEVELOPMENT_BUILD=1
-
-CFLAGS="-O2 -g -std=c++11 -mavx2 -fdiagnostics-absolute-paths -fno-threadsafe-statics"
-CLFAGS="${CFLAGS} -DDEVELOPMENT_BUILD=${DEVELOPMENT_BUILD}"
+CFLAGS="-g -std=c++11 -mavx2 -fdiagnostics-absolute-paths -fno-threadsafe-statics -I ../code/shared/"
 CFLAGS="${CFLAGS} $(sdl2-config --cflags)"
-CFLAGS="${CFLAGS} -Werror"
-CFLAGS="${CFLAGS} -Wall"
+CFLAGS="${CFLAGS} -Werror -Wall"
 CFLAGS="${CFLAGS} -Wno-missing-braces"
 CFLAGS="${CFLAGS} -Wno-writable-strings"
 CFLAGS="${CFLAGS} -Wno-char-subscripts"
-
-if [[ "$DEVELOPMENT_BUILD" == 1 ]]
-then
-    CFLAGS="${CFLAGS} -Wno-unused-variable"
-    CFLAGS="${CFLAGS} -Wno-unused-function"
-fi
+CFLAGS="${CFLAGS} -Wno-unused-variable"
+CFLAGS="${CFLAGS} -Wno-unused-function"
 
 LDFLAGS="$(sdl2-config --libs)"
 
 mkdir -p ../../build
 pushd ../../build > /dev/null
 
-clang $CFLAGS ../code/graphics/platform_sdl.cpp -o gfx $LDFLAGS
+clang $CFLAGS ../code/desktop/platform_sdl.cpp -DDEVELOPMENT_BUILD=1 -Og -o desktop_debug   $LDFLAGS
+clang $CFLAGS ../code/desktop/platform_sdl.cpp -DDEVELOPMENT_BUILD=0 -O2 -o desktop_release $LDFLAGS
 
 popd
